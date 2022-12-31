f02672b2d8e6df4e6e63178e75f73cab15c631b9
DESKTOP-T5BVQ56\gabry <gabry99rose@gmail.com>
2022-12-31 17:37:11 +0100

    set up base schermate finito

---
+++ src/Add.js
@@ -0,0 +1,22 @@
+import {useContext, useState} from "react";
+import {StateContext} from "./App";
+import {Link} from "react-router-dom";
+import {Button, Col, Container, Row} from "react-bootstrap";
+
+export default function Notifiche(){
+
+    const [state,dispatch] = useContext(StateContext)
+
+    return(
+        <Container fluid styile={{height:"79vh"}}>
+            <Row className="justify-content-md-center">
+                <Col xs={12} sm={12} md={3}>
+                    <Button variant={"dark"} style={{marginBottom:"1em"}}>
+                        <Link to={"/home"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>home</span></Link>
+                    </Button>
+                </Col>
+            </Row>
+        </Container>
+    )
+
+}
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import {useContext, useState} from "react";
 import {StateContext} from "./App";
 import {Link} from "react-router-dom";
-import {Button, Col, Container, Image, Row} from "react-bootstrap";
+import {Button, Card, Col, Container, Form, Image, Nav, Navbar, Row} from "react-bootstrap";
 import img_1 from "./img_1.png";
 
 export default function Home(){
@@ -9,76 +9,144 @@ export default function Home(){
     const [state,dispatch] = useContext(StateContext)
 
     return(
-        <Container fluid styile={{height:"79vh"}}>
-            <header>
-                    <Row>
-                        <Col style={{textAlign: "left"}}><Button variant={"light"}><Image src={img_1} alt={"Immagine del logo"} fluid style={{blockSize: "100px", marginBottom: "15px"}}/></Button></Col>
-                        <Col xs={12} sm={12} md={1}>
-                            <Button variant={"dark"} style={{marginBottom:"1em"}}><Link to={"/notifications"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>noti</span></Link></Button>
-                        </Col>
-                        <Col xs={1} sm={1} md={1}>
-                            <Button variant={"dark"} style={{marginBottom:"1em"}}><Link to={"/messages"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>mess</span></Link></Button>
-                        </Col>
-                        <Col xs={1} sm={1} md={1}>
-                            <Button variant={"dark"} style={{marginBottom:"1em"}}><Link to={"/tutorial"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>tuto</span></Link></Button>
-                        </Col>
-                    </Row>
-            </header>
-            <Row className="justify-content-md-center">
-                <Col xs={12} sm={12} md={3}>
+        <Container fluid style={{height:"79vh"}}>
+
+            <Navbar style={{overflow: "hidden", top: "0px", width: "100%"}} bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/home">APPy Family</Navbar.Brand>
+                    <Nav>
+                        <Nav.Link style={{textAlign: "right"}} href="/notifications">Notifications</Nav.Link>
+                        <Nav.Link style={{textAlign: "right"}} href="/messages">Messages</Nav.Link>
+                        <Nav.Link style={{textAlign: "right"}} href="/tutorial">Tutorial</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+
+            <Form className="d-flex">
+                <Form.Control type="search" placeholder="Search" aria-label="Search"/>
+                <Button variant="outline-success">Search</Button>
+            </Form>
+
+            <Row className="justify-content-sm-center">
+                <Col xs={3} sm={12} md={3}>
                     <Button variant={"danger"} style={{marginBottom:"1em"}}>
-                        <Link to={"/home"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>EVENTS</span></Link>
+                        <Link to={"/home"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>EVE</span></Link>
                     </Button>
                 </Col>
-                <Col xs={12} sm={12} md={3}>
+                <Col xs={3} sm={12} md={3}>
                     <Button variant={"dark"} style={{marginBottom:"1em"}}>
-                        <Link to={"/people"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>PEOPLE</span></Link>
+                        <Link to={"/people"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>PEO</span></Link>
                     </Button>
                 </Col>
-                <Col xs={12} sm={12} md={3}>
+                <Col xs={3} sm={12} md={3}>
                     <Button variant={"dark"} style={{marginBottom:"1em"}}>
-                        <Link to={"/blogs"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>BLOGS</span></Link>
+                        <Link to={"/blogs"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>BLO</span></Link>
                     </Button>
                 </Col>
-                <Col xs={12} sm={12} md={3}>
+                <Col xs={3} sm={12} md={3}>
                     <Button variant={"dark"} style={{marginBottom:"1em"}}>
-                        <Link to={"/hashtags"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>HASHTAGS</span></Link>
+                        <Link to={"/hashtags"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>HAS</span></Link>
                     </Button>
                 </Col>
             </Row>
 
-            <Row className="justify-content-md-center">
-                <Col xs={12} sm={12} md={3}>
+            <Row className="justify-content-sm-center">
+                <Col xs={6} sm={12} md={3}>
                     <Button variant={"danger"} style={{marginBottom:"1em"}}>
                         <Link to={"/home"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>RECOMMENDED</span></Link>
                     </Button>
                 </Col>
-                <Col xs={12} sm={12} md={3}>
+                <Col xs={6} sm={12} md={3}>
                     <Button variant={"dark"} style={{marginBottom:"1em"}}>
                         <Link to={"/nearme"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>NEAR ME</span></Link>
                     </Button>
                 </Col>
             </Row>
 
-            <Row className="justify-content-md-center">
-                <Col xs={12} sm={12} md={3}>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+
+            <Row className="justify-content-sm-center">
+                <Col xs={1} md={1}>
+                    <Button variant={"dark"} style={{marginBottom:"1em"}}>
+                        <Link to={"/add"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>+</span></Link>
+                    </Button>
+                </Col>
+            </Row>
+
+            <Row className="justify-content-sm-center">
+                <Col xs={3} sm={12} md={3}>
                     <Button variant={"danger"} style={{marginBottom:"1em"}}>
-                        <Link to={"/home"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>HOME</span></Link>
+                        <Link to={"/home"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>HOM</span></Link>
                     </Button>
                 </Col>
-                <Col xs={12} sm={12} md={3}>
+                <Col xs={3} sm={12} md={3}>
                     <Button variant={"dark"} style={{marginBottom:"1em"}}>
-                        <Link to={"/calendar"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>CALENDAR</span></Link>
+                        <Link to={"/calendar"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>CAL</span></Link>
                     </Button>
                 </Col>
-                <Col xs={12} sm={12} md={3}>
+                <Col xs={3} sm={12} md={3}>
                     <Button variant={"dark"} style={{marginBottom:"1em"}}>
-                        <Link to={"/profile"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>PROFILE</span></Link>
+                        <Link to={"/profile"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>PRO</span></Link>
                     </Button>
                 </Col>
-                <Col xs={12} sm={12} md={3}>
+                <Col xs={3
+                } sm={12} md={3}>
                     <Button variant={"dark"} style={{marginBottom:"1em"}}>
-                        <Link to={"/settings"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>SETTINGS</span></Link>
+                        <Link to={"/settings"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>SE</span></Link>
                     </Button>
                 </Col>
             </Row>
