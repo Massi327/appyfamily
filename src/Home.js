5aa58a4016b34595b0adddbc588fb0be4a9faa73
DESKTOP-GISG5PG\CS <s272462@studenti.polito.it>
2023-01-06 20:00:50 +0100

    prova eli

--- src/Home.js
+++ src/Home.js
@@ -8,12 +8,22 @@ import logo from './images/APPy family-logo.png';
 import notif from './images/notifications-icon.svg';
 import message from './images/messages-icon.svg';
 import help from './images/help-icon.svg'
+import events from './images/Events.svg'
+import people from './images/People.svg'
+import blogs from './images/Blogs.svg'
+import hashtags from './images/Hashtags.svg'
+import recbut from './images/recommended-button.svg'
+import nearme from './images/near-me.svg'
+import ellipse from './images/Ellipse-bottom-menu.svg'
+import home from './images/Home-icon.svg'
+import add from './images/Add.svg'
 
 export default function Home(){
 
     const [state,dispatch] = useContext(StateContext)
 
     return(
+        <Container>
         <Container className="container">
 
             <Navbar className="navigbar" style={{overflow: "hidden", top: "0px", width: "100%"}}>
@@ -30,44 +40,49 @@ export default function Home(){
 
             <Form className="d-flex">
                 <Form.Control type="search" placeholder="Search" aria-label="Search"/>
-                <Button variant="outline-success">Search</Button>
+                <Button className="search-button" variant="outline-success">Search</Button>
             </Form>
 
-            <Row className="justify-content-sm-center">
-                <Col xs={3} sm={12} md={3}>
-                    <Button variant={"danger"} style={{marginBottom:"1em"}}>
-                        <Link to={"/home"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>EVE</span></Link>
-                    </Button>
-                </Col>
-                <Col xs={3} sm={12} md={3}>
-                    <Button variant={"dark"} style={{marginBottom:"1em"}}>
-                        <Link to={"/people"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>PEO</span></Link>
-                    </Button>
-                </Col>
-                <Col xs={3} sm={12} md={3}>
-                    <Button variant={"dark"} style={{marginBottom:"1em"}}>
-                        <Link to={"/blogs"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>BLO</span></Link>
-                    </Button>
-                </Col>
-                <Col xs={3} sm={12} md={3}>
-                    <Button variant={"dark"} style={{marginBottom:"1em"}}>
-                        <Link to={"/hashtags"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>HAS</span></Link>
-                    </Button>
-                </Col>
-            </Row>
+            <Container className="search-icons">
+                <Row className="justify-content-sm-center">
+                    <Col xs={3} sm={12} md={3}>
 
-            <Row className="justify-content-sm-center">
-                <Col xs={6} sm={12} md={3}>
-                    <Button variant={"danger"} style={{marginBottom:"1em"}}>
-                        <Link to={"/home"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>RECOMMENDED</span></Link>
-                    </Button>
-                </Col>
-                <Col xs={6} sm={12} md={3}>
-                    <Button variant={"dark"} style={{marginBottom:"1em"}}>
-                        <Link to={"/nearme"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>NEAR ME</span></Link>
-                    </Button>
-                </Col>
-            </Row>
+                        <Link to={"/home"} style={{color: "white", textDecoration: "none"}}><span><img src={events} alt="Events" className="search-icon"/></span></Link>
+
+                    </Col>
+                    <Col xs={3} sm={12} md={3}>
+
+                        <Link to={"/people"} style={{color: "white", textDecoration: "none"}}><span><img src={people} alt="People" className="search-icon"/></span></Link>
+
+                    </Col>
+                    <Col xs={3} sm={12} md={3}>
+
+                        <Link to={"/blogs"} style={{color: "white", textDecoration: "none"}}><span><img src={blogs} alt="Blogs" className="search-icon"/></span></Link>
+
+                    </Col>
+                    <Col xs={3} sm={12} md={3}>
+
+                        <Link to={"/hashtags"} style={{color: "white", textDecoration: "none"}}><span><img src={hashtags} alt="Hashtags" className="search-icon"/></span></Link>
+
+                    </Col>
+                </Row>
+            </Container>
+
+            <Container className="cont-menu-rec-nearme">
+                <Row className="menu-rec-nearme">
+
+                    <Col xs={6} sm={12} md={3}>
+                            <Link to={"/home"} ><img src={recbut} alt="Recommended" className="icon" style={{marginTop:"5px"}}/></Link>
+                    </Col>
+                    <Col xs={6} sm={12} md={3}>
+                            <Link to={"/nearme"}><img src={nearme} alt="Near me" className="icon" style={{marginTop:"10px"}}/></Link>
+                    </Col>
+
+
+                </Row>
+            </Container>
+
+        </Container>
 
             <Card style={{ width: '18rem' }}>
                 <Card.Img variant="top" src="holder.js/100px180" />
@@ -127,35 +142,37 @@ export default function Home(){
 
             <Row className="justify-content-sm-center">
                 <Col xs={1} md={1}>
-                    <Button variant={"dark"} style={{marginBottom:"1em"}}>
-                        <Link to={"/add"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>+</span></Link>
-                    </Button>
+                    <Link to={"/add"} style={{color: "white", textDecoration: "none"}}><span><img src={add} alt="Add" className="search-icon"/></span></Link>
                 </Col>
             </Row>
 
-            <Row className="justify-content-sm-center">
-                <Col xs={3} sm={12} md={3}>
-                    <Button variant={"danger"} style={{marginBottom:"1em"}}>
-                        <Link to={"/home"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>HOM</span></Link>
-                    </Button>
-                </Col>
-                <Col xs={3} sm={12} md={3}>
-                    <Button variant={"dark"} style={{marginBottom:"1em"}}>
-                        <Link to={"/calendar"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>CAL</span></Link>
-                    </Button>
-                </Col>
-                <Col xs={3} sm={12} md={3}>
-                    <Button variant={"dark"} style={{marginBottom:"1em"}}>
-                        <Link to={"/profile"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>PRO</span></Link>
-                    </Button>
-                </Col>
-                <Col xs={3
-                } sm={12} md={3}>
-                    <Button variant={"dark"} style={{marginBottom:"1em"}}>
-                        <Link to={"/settings"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>SE</span></Link>
-                    </Button>
-                </Col>
-            </Row>
+
+            <Container className="navigbar-bottom">
+                <Row className="justify-content-sm-center">
+                    <Col xs={3} sm={12} md={3}>
+                        <Link to={"/home"} style={{color: "white", textDecoration: "none"}}><span><img src={home} alt="Home" className="search-icon"/></span></Link>
+
+                    </Col>
+                    <Col xs={3} sm={12} md={3}>
+                        <Button variant={"dark"} style={{marginBottom:"1em"}}>
+                            <Link to={"/calendar"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>CAL</span></Link>
+                        </Button>
+                    </Col>
+                    <Col xs={3} sm={12} md={3}>
+                        <Button variant={"dark"} style={{marginBottom:"1em"}}>
+                            <Link to={"/profile"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>PRO</span></Link>
+                        </Button>
+                    </Col>
+                    <Col xs={3
+                    } sm={12} md={3}>
+                        <Button variant={"dark"} style={{marginBottom:"1em"}}>
+                            <Link to={"/settings"} style={{color: "white", textDecoration: "none"}}><span style={{margin: "0.5em"}}>SE</span></Link>
+                        </Button>
+                    </Col>
+                </Row>
+            </Container>
+
+
         </Container>
     )
 
