3adb77b621d81caa3cc4cb1ffa3133e0a1d35558
DESKTOP-GISG5PG\CS <s272462@studenti.polito.it>
2023-01-16 14:19:50 +0100

    modifiche tutto

--- src/Home.js
+++ src/Home.js
@@ -25,10 +25,19 @@ import clock from './images/Clock.svg'
 import map from './images/Map.svg'
 import imgcard2 from './images/Image-event-2.svg'
 import imgcard1 from './images/Image-event-1.svg'
+import imgcard3 from './images/Image-event-3.svg'
+import imgcard4 from './images/Image-event-4.svg'
+import imgcard5 from './images/Image-event-5.svg'
 import Navigbar from "./components/navbar-search";
 import NavigbarBottom from "./components/navbar-bottom";
 import AddButton from "./components/add-button";
 import eventsselected from "./images/Events-selezionato.svg";
+import interested from "./images/ic_round-star-border.svg";
+import participate from "./images/ic_round-check-circle-outline.svg";
+import whosgoing from "./images/material-symbols_emoji-people-rounded.svg";
+import dropdown from "./images/gridicons_dropdown.svg"
+import popupsport from "./images/image 1.svg"
+import threedots from "./images/ph_dots-three-vertical-bold.svg"
 
 import 'moment/locale/en-gb';
 import moment from "moment/moment";
@@ -78,29 +87,51 @@ export default function Home(){
 
                 <Modal show={show} onHide={handleClose} backdrop={"static"} centered>
                     <Modal.Dialog>
-                        <Modal.Header>
+
+
+                        <Modal.Header closeButton>
+                            <Card style={{backgroundColor:"#4b7bf8", color:"white"}}>
+                                <Card.Body>
+                                    <Card.Text className="event-month-popup" style={{textAlign: 'center'}}>                      {prenotazione.filter(p => p.key===state.id).map(m=>moment(m.dataStart).locale('en').format('MMM').toUpperCase())}
+                                    </Card.Text>
+                                    <Card.Text className="event-day-popup" style={{textAlign: 'center'}}>                      {prenotazione.filter(p => p.key===state.id).map(m=>moment(m.dataStart).locale('en').format('D').toUpperCase())}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
                             <Modal.Title className="modal-title-1">{prenotazione.filter(p => p.key===state.id).map(m=>m.titolo)}</Modal.Title>
                         </Modal.Header>
 
                         <Modal.Body className="modal-subtitle-1">
-                            <p>{prenotazione.filter(p => p.key===state.id).map(m=>m.about)}</p>
-                            <p>{prenotazione.filter(p => p.key===state.id).map(m=>m.address)}</p>
-                            <p>{prenotazione.filter(p => p.key===state.id).map(m=>moment(m.dataStart).locale('en').format('D MMM YYYY'))}, {prenotazione.filter(p => p.key===state.id).map(m=>moment(m.dataStart).locale('en').format('h:mm a'))} - {prenotazione.filter(p => p.key===state.id).map(m=>moment(m.dataEnd).locale('en').format('h:mm a'))}</p>
 
+                            <img src={popupsport} style={{marginBottom:"0.5em", width:"22em"}}/>
+
+                            <p className="event-subsubtitle-3"> <img src={clock} className="icon"/> {prenotazione.filter(p => p.key===state.id).map(m=>m.address)}</p>
+                            <p className="event-subsubtitle-3"> <img src={map} className="icon"/> {prenotazione.filter(p => p.key===state.id).map(m=>moment(m.dataStart).locale('en').format('D MMM YYYY'))}, {prenotazione.filter(p => p.key===state.id).map(m=>moment(m.dataStart).locale('en').format('h:mm a'))} - {prenotazione.filter(p => p.key===state.id).map(m=>moment(m.dataEnd).locale('en').format('h:mm a'))}</p>
+                            <p className="about">About</p>
+                            <p>{prenotazione.filter(p => p.key===state.id).map(m=>m.about)}</p>
                         </Modal.Body>
 
                         <Modal.Footer>
-                            <Button style={{borderColor:"#eb506c", color:"#eb506c", borderWidth:"2px", backgroundColor:"#f5f5f5", borderRadius:"10px", marginRight:"0.5em"}}
+                            <Button style={{fontSize:"13px",borderColor:"#eb506c", color:"#eb506c", borderWidth:"2px", backgroundColor:"#f5f5f5", borderRadius:"10px", marginRight:"0em"}}
                                     onClick={handleClose}
-                            >Cancel</Button>
+                            ><img src={interested}/> Interested</Button>
+                            <Button style={{fontSize:"13px", borderColor:"#eb506c", color:"#eb506c", borderWidth:"2px", backgroundColor:"#f5f5f5", borderRadius:"10px", marginRight:"0em"}}
+                                    onClick={handleClose}
+                            ><img src={participate}/> Participate</Button>
+                            <Button style={{fontSize:"13px",borderColor:"#eb506c", color:"#eb506c", borderWidth:"2px", backgroundColor:"#f5f5f5", borderRadius:"10px", marginRight:"0em"}}
+                                    onClick={handleClose}
+                            ><img src={whosgoing}/> Who's going<img src={dropdown} style={{marginRight:"-10px"}}/></Button>
                         </Modal.Footer>
                     </Modal.Dialog>
                 </Modal>
 
+
+
+
                 {events.slice(0).reverse().map( p =>
 
                     <Card className="post" key={p.key} style={{ height: '8rem', marginBottom: '1em' , borderRadius: '10px',borderWidth: '0', flexDirection: 'row'}} onClick={()=> {handleShow(); dispatch(selected(p.key))}}>
-                        <Card.Img className="cardimg" src={imgcard1} style={{height: '8em', width: '10rem', verticalAlign:'center'}} />
+                        <Card.Img className="cardimg" src={imgcard4} style={{height: '8em', width: '10rem', verticalAlign:'center'}} />
                         <Card.Body>
                             <Card.Text className="event-time-1" style={{textAlign: 'left'}}>{moment(p.dataStart).locale('en').format('MMM D').toUpperCase()} • {moment(p.dataStart).locale('en').format('h:mm a').toUpperCase()}</Card.Text>
                             <Card.Title className="event-title-1" style={{textAlign: 'left'}}>{p.titolo}</Card.Title>
@@ -121,13 +152,20 @@ export default function Home(){
             <Card className="post" style={{ height: '8rem', marginBottom: '1em' , borderRadius: '10px',borderWidth: '0', flexDirection: 'row'}}>
                 <Card.Img className="cardimg" src={imgcard1} style={{height: '8em', width: '10rem', verticalAlign:'center'}} />
                 <Card.Body>
-                    <Card.Text className="event-time-1" style={{textAlign: 'left'}}>JAN 7 • 10AM</Card.Text>
-                    <Card.Title className="event-title-1" style={{textAlign: 'left'}}>Play in the Park</Card.Title>
+
+                    <Card.Text className="event-time-1">
+                        <Row>
+                            <Col>
+                                <h5 style={{textAlign:"left"}}>JAN 7 • 10AM </h5>
+                            </Col>
+                            <Col xs={2}>
+                                <img src={threedots} style={{textAlign:"right"}}/>
+                            </Col>
+                        </Row>
+                        </Card.Text>
+                        <Card.Title className="event-title-1" style={{textAlign: 'left'}}>Play in the Park</Card.Title>
                     <Card.Text className="event-subtitle-1" style={{textAlign: 'left'}}>
                         Hosted by the Muse Th.
-                    </Card.Text>
-                    <Card.Text className="event-subsubtitle-1" style={{textAlign: 'left'}}>
-                        Public
                     </Card.Text>
                     <Card.Text className="event-subsubtitle-2" style={{textAlign: 'left'}}>
                         <img src={clock} alt="Near me" className="icon"/> 7 Jan 2023, 10AM - 11AM
@@ -146,9 +184,6 @@ export default function Home(){
                         <Card.Text className="event-subtitle-1" style={{textAlign: 'left'}}>
                             Hosted by Color Notes
                         </Card.Text>
-                        <Card.Text className="event-subsubtitle-1" style={{textAlign: 'left'}}>
-                            Public
-                        </Card.Text>
                         <Card.Text className="event-subsubtitle-2" style={{textAlign: 'left'}}>
                             <img src={clock} alt="Near me" className="icon"/> 7 Jan 2023, 10AM - 11AM
                         </Card.Text>
@@ -160,42 +195,36 @@ export default function Home(){
 
 
                 <Card className="post" style={{height: '8rem', marginBottom: '1em' , borderRadius: '10px',borderWidth: '0', flexDirection: 'row'}}>
-                    <Card.Img className="cardimg" src={imgcard1} style={{height: '8em', width: '10rem'}} />
+                    <Card.Img className="cardimg" src={imgcard5} style={{height: '8em', width: '10rem'}} />
                     <Card.Body>
-                        <Card.Text className="event-time-1" style={{textAlign: 'left'}}>JAN 7 • 10AM</Card.Text>
-                        <Card.Title className="event-title-1" style={{textAlign: 'left'}}>Play in the Park</Card.Title>
+                        <Card.Text className="event-time-1" style={{textAlign: 'left'}}>JAN 8 • 5PM</Card.Text>
+                        <Card.Title className="event-title-1" style={{textAlign: 'left'}}>Joe's Party</Card.Title>
                         <Card.Text className="event-subtitle-1" style={{textAlign: 'left'}}>
-                            Hosted by the Muse Th.
-                        </Card.Text>
-                        <Card.Text className="event-subsubtitle-1" style={{textAlign: 'left'}}>
-                            Public
+                            Hosted by the Mia Johnson
                         </Card.Text>
                         <Card.Text className="event-subsubtitle-2" style={{textAlign: 'left'}}>
-                            <img src={clock} alt="Near me" className="icon"/> 7 Jan 2023, 10AM - 11AM
+                            <img src={clock} alt="Near me" className="icon"/> 7 Jan 2023, 5PM - 8PM
                         </Card.Text>
                         <Card.Text className="event-subsubtitle-2" style={{textAlign: 'left'}}>
-                            <img src={map} alt="Near me" className="icon"/> 112 Barrack Street, NR3 1TX, UK
+                            <img src={map} alt="Near me" className="icon"/> 113 Flams Close, CB4 2TY, UK
                         </Card.Text>
                     </Card.Body>
                 </Card>
 
 
                 <Card className="post" style={{height: '8rem', marginBottom: '1em' , borderRadius: '10px',borderWidth: '0', flexDirection: 'row'}}>
-                    <Card.Img className="cardimg" src={imgcard1} style={{height: '8em', width: '10rem'}} />
+                    <Card.Img className="cardimg" src={imgcard4} style={{height: '8em', width: '10rem'}} />
                     <Card.Body>
                         <Card.Text className="event-time-1" style={{textAlign: 'left'}}>JAN 7 • 10AM</Card.Text>
-                        <Card.Title className="event-title-1" style={{textAlign: 'left'}}>Play in the Park</Card.Title>
+                        <Card.Title className="event-title-1" style={{textAlign: 'left'}}>Soccer U10</Card.Title>
                         <Card.Text className="event-subtitle-1" style={{textAlign: 'left'}}>
-                            Hosted by the Muse Th.
-                        </Card.Text>
-                        <Card.Text className="event-subsubtitle-1" style={{textAlign: 'left'}}>
-                            Public
+                            Hosted by the MegaSport
                         </Card.Text>
                         <Card.Text className="event-subsubtitle-2" style={{textAlign: 'left'}}>
-                            <img src={clock} alt="Near me" className="icon"/> 7 Jan 2023, 10AM - 11AM
+                            <img src={clock} alt="Near me" className="icon"/> 7 Jan 2023, 8PM - 9PM
                         </Card.Text>
                         <Card.Text className="event-subsubtitle-2" style={{textAlign: 'left'}}>
-                            <img src={map} alt="Near me" className="icon"/> 112 Barrack Street, NR3 1TX, UK
+                            <img src={map} alt="Near me" className="icon"/> 13 Congo Street, PT3 1MX, UK
                         </Card.Text>
                     </Card.Body>
                 </Card>
